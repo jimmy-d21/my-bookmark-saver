f1e1846ebe361957d7c20fbd766165af4cba6b52
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-10-05 14:30:36 +0800

    Create a function for adding bookmark

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ const bookmarkUrlInput = document.getElementById("bookmark-url");
 const bookmarkAddButton = document.getElementById("add-bookmark-btn");
 const bookmarkList = document.getElementById("bookmark-list");
 
+
+// Event Listeners
+bookmarkAddButton.addEventListener("click", addBookmark);
+
+function addBookmark() {
+    const name = bookmarkNameInput.value.trim();
+    const url = bookmarkUrlInput.value.trim();
+
+    if(!name || !url) {
+        alert("Please provide both name and URL.");
+        return;
+    } else if(!name.startsWith("http://") && !url.startsWith("https://")) {
+        alert("Please provide a valid URL.");
+        return;
+    }
+
+    const bookmark = {
+        name,
+        url
+    }
+   console.log(bookmark);
+    bookmarkNameInput.value = "";
+    bookmarkUrlInput.value = "";
+}
+
