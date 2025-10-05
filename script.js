1294deaaac7eafaceb7479872d083cbbf60bed79
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-10-05 14:27:21 +0800

    declare all the doms

---
+++ script.js
@@ -0,0 +1,6 @@
+// Dom
+const bookmarkNameInput = document.getElementById("bookmark-name");
+const bookmarkUrlInput = document.getElementById("bookmark-url");
+const bookmarkAddButton = document.getElementById("add-bookmark-btn");
+const bookmarkList = document.getElementById("bookmark-list");
+
